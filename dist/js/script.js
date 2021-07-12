1d120b6f543ef9ca36e9a0c381dd9fa6186a1690
Яровлав <info@i-com.by>
2021-07-12 15:08:45 +0300

    Timer (установка таймера)

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 window.addEventListener('DOMContentLoaded', () => {
 
+   //Start Tabs ************************************************************************************************************************
 
    const tabsParent = document.querySelector('.tabheader__items'),
       tabs = document.querySelectorAll('.tabheader__item'),
@@ -39,8 +40,61 @@ window.addEventListener('DOMContentLoaded', () => {
       }
    });
 
+   //End Tabs **************************************************************************************************************************
 
+   //Start Timer ***********************************************************************************************************************
 
+   const deadLine = '2021-07-19';
+
+   function getTimeRemaining(endtime) {
+      const t = Date.parse(endtime) - Date.parse(new Date()),
+         days = Math.floor(t / (1000 * 60 * 60 * 24)),
+         hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+         minutes = Math.floor((t / 1000 / 60) % 60),
+         seconds = Math.floor((t / 1000) % 60);
+      
+      return {
+         total: t,
+         days: days,
+         hours: hours,
+         minutes: minutes,
+         seconds: seconds
+      };
+   }
+
+   function getZero(num) {
+      if (num >= 0 && num <10) {
+         return `0${num}`;
+      } else {
+         return num;
+      }
+   }
+
+   function setClock(selector, endtime) {
+      const timer = document.querySelector(selector),
+         days = timer.querySelector('#days'),
+         hours = timer.querySelector('#hours'),
+         minutes = timer.querySelector('#minutes'),
+         seconds = timer.querySelector('#seconds'),
+         timeInterval = setInterval(updateClock, 1000);
+      
+      updateClock();
+      
+      function updateClock() {
+         const t = getTimeRemaining(endtime);
+
+         days.innerHTML = getZero(t.days);
+         hours.innerHTML = getZero(t.hours);
+         minutes.innerHTML = getZero(t.minutes);
+         seconds.innerHTML = getZero(t.seconds);
+
+         if (t.total <= 0) {
+            clearInterval(timeInterval);
+         }
+      }
+   }
+
+   setClock('.timer', deadLine);
 
 
 });
