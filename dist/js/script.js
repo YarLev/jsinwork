73091676aac64f31a582d43b2e3a803a07a9cd29
Яровлав <info@i-com.by>
2021-07-16 16:51:31 +0300

    Forms AJAX
    Отправка формы на сервер

--- dist/js/script.js
+++ dist/js/script.js
@@ -224,6 +224,65 @@ window.addEventListener('DOMContentLoaded', () => {
    ).render();
 
 
+   // Forms *********************************************************************************
+
+   const forms = document.querySelectorAll('form');
+
+   const message = {
+      loading: 'Загрузка',
+      sucsess: 'Спасибо! Мы свяжемся с Вами через 10 минут',
+      failure: 'Что-то пошло не так...'
+   };
+
+   forms.forEach(item => {
+      postData(item);
+   });
+
+   function postData(form) {
+      form.addEventListener('submit', (event) => {
+         event.preventDefault();
+
+         const statusMessage = document.createElement('div');
+         statusMessage.classList.add('status');
+         statusMessage.textContent = message.loading;
+         form.append(statusMessage);
+
+
+         const request = new XMLHttpRequest();
+
+         request.open('POST', 'server.php');
+
+         request.setRequestHeader('Content-type', 'application/json');
+         const formData = new FormData(form);
+
+         const object = {};
+         formData.forEach(function (value, key){
+            object[key] = value;
+         });
+
+         const json = JSON.stringify(object);
+
+         request.send(json);
+
+         request.addEventListener('load', () => {
+            if (request.status === 200) {
+               statusMessage.textContent = message.sucsess;
+               form.reset();
+               setTimeout(() => {
+                  statusMessage.remove();
+               }, 2000);
+            } else {
+               statusMessage.textContent = message.failure;
+            }
+         });
+
+
+      });
+   }
+
+
+
+
 
 
 
