3a3cf6610b1783619555d923af0a3ed2a5dd2982
Яровлав <info@i-com.by>
2021-07-19 16:45:09 +0300

    insert DB.json
    Добавил npm и базу данных для получения динамических карточек

--- dist/js/script.js
+++ dist/js/script.js
@@ -44,7 +44,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
    // Start Timer *******************************************************************************************
 
-   const deadLine = '2021-07-19';
+   const deadLine = '2021-07-21';
 
    function getTimeRemaining(endtime) {
       const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -191,34 +191,21 @@ window.addEventListener('DOMContentLoaded', () => {
       }
    }
 
-   new MenuClass( //используется такая запись, если класс используется только однажды
-      "img/tabs/vegy.jpg",
-      "vegy",
-      'Меню "Фитнес"',
-      'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-      9,
-      ".menu .container",
-   ).render();
+   const getResource = async (url) => {
+      const res = await fetch(url);
 
-   new MenuClass( //используется такая запись, если класс используется только однажды
-      "img/tabs/elite.jpg",
-      "elite",
-      'Меню “Премиум”',
-      'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты, злаки, говядина – это полноценное ресторанное меню без похода в ресторан!',
-      22,
-      ".menu .container",
-      'menu__item'
-   ).render();
+      if (!res.ok) {
+         throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+      }
+      return await res.json();
+   };
 
-   new MenuClass( //используется такая запись, если класс используется только однажды
-      "img/tabs/post.jpg",
-      "post",
-      'Меню "Постное"',
-      'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-      17,
-      ".menu .container",
-      'menu__item'
-   ).render();
+   getResource('http://localhost:3000/menu')
+      .then(data => {
+         data.forEach(({img, altimg, title, descr, price}) => {
+            new MenuClass(img, altimg, title, descr, price, '.menu .container').render();
+         });
+      });
 
 
    // Forms *********************************************************************************
@@ -232,10 +219,22 @@ window.addEventListener('DOMContentLoaded', () => {
    };
 
    forms.forEach(item => {
-      postData(item);
+      bindPostData(item);
    });
 
-   function postData(form) {
+   const postData = async (url, data) => {
+      const res = await fetch(url, {
+         method: 'POST',
+         headers: {
+            'Content-type': 'application/json'
+         },
+         body: data
+      });
+
+      return await res.json();
+   };
+
+   function bindPostData(form) {
       form.addEventListener('submit', (event) => {
          event.preventDefault();
 
@@ -249,28 +248,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
          const formData = new FormData(form);
 
-         const object = {};
-         formData.forEach(function (value, key) {
-            object[key] = value;
-         });
+         const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-         fetch('server.php', {
-            method: 'POST',
-            headers: {
-               'Content-type': 'application/json'
-            },
-            body: JSON.stringify(object)
-         })
-         .then(data => data.text())
-         .then(data => {
-            console.log(data);
-            showThanksModal(message.sucsess);
-            statusMessage.remove();
-         }).catch(() => {
-            showThanksModal(message.failure);
-         }).finally(() => {
-            form.reset();
-         });
+         postData('http://localhost:3000/requests', json)
+            .then(data => {
+               console.log(data);
+               showThanksModal(message.sucsess);
+               statusMessage.remove();
+            }).catch(() => {
+               showThanksModal(message.failure);
+            }).finally(() => {
+               form.reset();
+            });
       });
    }
 
@@ -299,7 +288,5 @@ window.addEventListener('DOMContentLoaded', () => {
    }
 
    // Forms *********************************************************************************
-
-
 
 });
